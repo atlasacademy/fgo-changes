f50e47db26654a91ad5b5dd8c483eea91ac5c5eb
minhducsun2002 <minhducsun2002@gmail.com>
2020-08-30 22:47:06 +0700

    Ensure we run in a clean directory

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { config } from 'dotenv'; config();
 import { join } from 'path';
-
 import { prepareRepository } from './repository';
 import { diff } from './diffBin';
 import { diffMaster } from './diffMaster';
 import { mstUpdate } from './updateMaster';
 import { dump } from './dump';
+import rimraf from 'rimraf';
 
 const region = process.argv[2] || process.env.REGION
 const path = join(__dirname, '..', `work`, `fgo-game-data-${region}`);
-
+// ensure a clean directory
+rimraf.sync(path);
 Promise.resolve()
     .then(() => prepareRepository(process.env.REPO, region, path))
     .then(() => diff(path))
