95e78c1f5810b10882457467281bc38eeeb137bb
squaresmile <squaresmile@protonmail.com>
2021-03-21 19:35:43 -0400

    Send both mission details and machine descriptions for master missions

--- src/updateMasterMission.ts
+++ src/updateMasterMission.ts
@@ -49,61 +49,72 @@ export async function updateMasterMission (m : Map<string, any[]>, region : stri
         process.stdout.write(`New master mission found. Dispatching updates... `);
         let [token, id] = process.env.WEBHOOK.split('/').reverse()
         let client = new WebhookClient(id, token);
-        await client.send('', {
-            username: `FGO Changelog | ${region}`,
-            avatarURL: 'https://apps.atlasacademy.io/db/logo192.png',
-            embeds: [
-                new MessageEmbed()
-                    .setTitle(`Master missions`)
-                    .addFields([{
-                        name: `Time`,
-                        value: '```FROM | ' + `${new Date(changes[0].startedAt * 1000).toUTCString()}`
-                                + '\n TO  | ' + `${new Date(changes[0].endedAt * 1000).toUTCString()}`
-                                + '```'
-                    }, {
-                        name: `Missions`,
-                        value: changes
-                            .sort((a, b) => a.id - b.id)
-                            .map(a => {
-                                let mstEventMissionCondition = missionConditions.get(a.id);
-                                let _condType = condTypes[mstEventMissionCondition.condType];
-                                let { detail } = a;
-                                let { targetNum } = mstEventMissionCondition;
-                                switch (_condType) {
-                                    case 'MISSION_CONDITION_DETAIL': {
-                                        let details = (mstEventMissionCondition.targetIds as number[])
-                                            .map(id => missionConditionDetails.get(id));
-                                        let parsedDetails = details.map(_ => {
-                                            let { addTargetIds, targetIds } = _;
-                                            switch (_.missionCondType) {
-                                                case DetailCondType.ENEMY_INDIVIDUALITY_KILL_NUM:
-                                                    return `Kill ${targetNum} ${
-                                                        orConcat(targetIds.map((trait : number) => `[Trait : ${trait}]`))
-                                                    } enem${
-                                                        targetNum > 1 ? 'ies' : 'y'
-                                                    }`;
-                                                case DetailCondType.QUEST_CLEAR_NUM_2:
-                                                    return `Clear ${targetNum} quest${targetNum > 1 ? 's' : ''}`;
-                                                case DetailCondType.DEFEAT_SERVANT_CLASS:
-                                                    return `Defeat ${targetNum} ${
-                                                        orConcat(targetIds.map((classId : number) => resolveClass(classId)))
-                                                    } servants${addTargetIds.length ? ` with ${
-                                                        orConcat(addTargetIds.map((trait : number) => `[Trait : ${trait}]`))
-                                                    }` : ''}`
-                                                default:
-                                                    return Object.keys(DetailCondType)
-                                                        .filter(key => isNaN(+key))
-                                                        .find(key => DetailCondType[key as keyof typeof DetailCondType] === _.missionCondType);
-                                            }
-                                        });
-                                        detail = parsedDetails.join(', ')
-                                    }
-                                }
-                                return `\`${a.id}\` | ${detail}`;
-                            }).join('\n')
-                    }])
-            ]
-        });
+
+        const condDetails = changes.map(a => `- ${a.detail}`).join('\n');
+
+        const condDescriptions = changes
+            .sort((a, b) => a.id - b.id)
+            .map(a => {
+                let mstEventMissionCondition = missionConditions.get(a.id);
+                let _condType = condTypes[mstEventMissionCondition.condType];
+                let { detail } = a;
+                let { targetNum } = mstEventMissionCondition;
+                switch (_condType) {
+                    case 'MISSION_CONDITION_DETAIL': {
+                        let details = (mstEventMissionCondition.targetIds as number[])
+                            .map(id => missionConditionDetails.get(id));
+                        let parsedDetails = details.map(_ => {
+                            let { addTargetIds, targetIds } = _;
+                            switch (_.missionCondType) {
+                                case DetailCondType.ENEMY_INDIVIDUALITY_KILL_NUM:
+                                    return `Kill ${targetNum} ${
+                                        orConcat(targetIds.map((trait : number) => `[Trait : ${trait}]`))
+                                    } enem${
+                                        targetNum > 1 ? 'ies' : 'y'
+                                    }`;
+                                case DetailCondType.QUEST_CLEAR_NUM_2:
+                                    return `Clear ${targetNum} quest${targetNum > 1 ? 's' : ''}`;
+                                case DetailCondType.DEFEAT_SERVANT_CLASS:
+                                    return `Defeat ${targetNum} ${
+                                        orConcat(targetIds.map((classId : number) => resolveClass(classId)))
+                                    } servants${addTargetIds.length ? ` with ${
+                                        orConcat(addTargetIds.map((trait : number) => `[Trait : ${trait}]`))
+                                    }` : ''}`
+                                default:
+                                    return Object.keys(DetailCondType)
+                                        .filter(key => isNaN(+key))
+                                        .find(key => DetailCondType[key as keyof typeof DetailCondType] === _.missionCondType);
+                            }
+                        });
+                        detail = parsedDetails.join(', ')
+                    }
+                }
+                return `\`${a.id}\` | ${detail}`;
+            }).join('\n');
+
+        const masterMissionMessage = (descriptions: string) => {
+            return {
+                username: `FGO Changelog | ${region}`,
+                avatarURL: 'https://apps.atlasacademy.io/db/logo192.png',
+                embeds: [
+                    new MessageEmbed()
+                        .setTitle(`Master missions`)
+                        .addFields([{
+                            name: `Time`,
+                            value: '```FROM | ' + `${new Date(changes[0].startedAt * 1000).toUTCString()}`
+                                    + '\n TO  | ' + `${new Date(changes[0].endedAt * 1000).toUTCString()}`
+                                    + '```'
+                        }, {
+                            name: `Missions`,
+                            value: descriptions
+                        }])
+                ]
+            }
+        }
+
+        await client.send('', masterMissionMessage(condDetails));
+        await client.send('', masterMissionMessage(condDescriptions));
+
         console.log(`Done.`);
         client.destroy();
     }
