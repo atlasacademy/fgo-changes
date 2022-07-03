c3335812fbbe7dca3ab823de9b2fb74f889e852f
squaresmile <squaresmile@protonmail.com>
2022-07-03 22:04:53 +0700

    Put token in repo url

--- src/repository.ts
+++ src/repository.ts
@@ -2,22 +2,12 @@ import { execFileSync } from 'child_process';
 
 export async function prepareRepository(url : string, region : string, dir : string) {
     execFileSync('git', [
-        '-c', `credential.username=${process.env.TOKEN}`,
-
         'clone',
-
-        '-b', region,
-        url,
-
+        '--branch', region,
+        `https://${process.env.TOKEN}@${url.replace("https://", "")}`,
         dir,
         '--depth', '2',
-
         '--single-branch'
-    ], {
-        env: {
-            ...process.env,
-            'GIT_ASKPASS': 'echo'
-        }
-    })
+    ])
     console.log(`Cloned repository to ${dir}.`)
 }
